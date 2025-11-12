922ffd326570fa469f643126e253224ef33e0f37
hyunjun <nujnuyh@gmail.com>
2025-11-12 19:03:16 +0900

    init main branch

---
+++ back/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+# #CORS
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],  # 배포할 땐 실제 프론트 도메인으로 제한
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+#endpoint
+@app.post('/sentiment')
+async def translate_sentiment(prompt:str):
+    return {"prompt":prompt}
+
---
+++ back/service.py
@@ -0,0 +1,33 @@
+from transformers import pipeline
+import onnxruntime
+
+translator = pipeline("translation", 
+                      model="Helsinki-NLP/opus-mt-tc-bible-big-mul-mul")
+
+translated = translator(">>kr<< You'd better not speak to Tom about that.")
+#text
+print(translated, translated[0]['translation_text'])
+
+# examples
+# src = [
+#     "Pedro Pascal leads the team back to the retro-future in colorful new version.",
+#     "David Corenswet and Rachel Brosnahan soar, but the script is kryptonite.",
+#     "Scarlett Johansson and Jonathan Bailey help the franchise roar back to life.",
+#     "This musical adaptation hits the right notes, but lacks visual variety.",
+#     "Ridley Scott sequel is epic old-fashioned movie-making with a star turn from Paul Mescal.",
+#     "Ana de Armas kills busloads of people in giddy, ruthless, orgy of violence.",
+#     "Clint Bentley's soft-spoken Western is one of the most quietly beautiful films of the year.",
+#     "Dwayne Johnson's Oscar effort is missing teeth, but not from getting punched in the face.",
+#     "Francis Ford Coppola's Megalopolis is bloated and unforgivably dull.",
+#     "The absolute authority and force of Daniel Day-Lewis carries this movie in the end, and what a pleasure to see his return to the screen.",
+# ]
+
+
+sentimental = pipeline('sentiment-analysis',
+                       model= "nlptown/bert-base-multilingual-uncased-sentiment")
+
+# [{'translation_text': "Pedro Pascalqa qatiqnintam reto-fuerzaman pusan, huk rikchaq rikch'ayniyoq rikch'ayniyoq rikch'ayniyoqman."}] 
+
+# [{'label': '1 star', 'score': 0.5432569980621338}]
+
+translated= 
